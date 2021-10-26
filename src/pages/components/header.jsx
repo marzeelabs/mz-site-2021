32e3769250ba985efc1501adb5beb98929ca3e68
Pedro Santos <pedro@marzeelabs.org>
2021-10-26 18:06:23 +0100

    Header start + Assets start + further components dev

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,11 +1,38 @@
+// import Logo from '../../../public/assets/mz_logo_horizontal.svg';
+
 const Header = () => { 
   console.log("Yo");
 
 
   return (
   <>
-    <header>
-      <h2>Header Imported</h2>
+    <header className="header">
+      <div className="header-content">
+        {/* <Logo /> */}
+        <h2>Header Logo</h2>
+
+        <ul className="header-menu">
+          <li className="header-menu__item">
+            <a className="header-menu__link" href="#">About</a>
+          </li>
+
+          <li className="header-menu__item">
+            <a className="header-menu__link" href="#">Services</a>
+          </li>
+
+          <li className="header-menu__item">
+            <a className="header-menu__link" href="#">Case Studies</a>
+          </li>
+
+          <li className="header-menu__item">
+            <a className="header-menu__link" href="#">Blog</a>
+          </li>
+        </ul>
+      </div>
+
+
+
+
     </header>
    
   </>

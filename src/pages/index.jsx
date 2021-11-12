2470ed708393691e113ccfb0aac77ae633f6a5e6
Pedro Santos <pedro@marzeelabs.org>
2021-11-12 12:39:02 +0000

    Theme development [homepage] - WIP

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,121 +1,125 @@
 import Header from './components/header';
+import HomepageIntro from './homepage-blocks/homepage-intro';
+import HomepageIntroCards from './homepage-blocks/homepage-intro-cards';
 const Home = () => (
   <>
     <Header />
     <main>
       <section className="homepage-intro__wrapper">
 
-        <div className="homepage-intro__container">
+        <HomepageIntro />
+        <HomepageIntroCards />
 
-          <div className="homepage-intro__content">
-            <h1 className="title-display-large">
-              We're<br />Marzee
-            </h1>
-            <h2 className="title-heading-3">
-              Bespoke web development services for bold and dynamic web platforms
-            </h2>
-
-            <button className="button-main" type="button" >Reach Out!</button>
-          </div>
-
-          <div className="homepage-intro__media">
-            <img
-              src="/assets/hp_illustration_top.png"
-              alt="intro illustration"
-              width="300"
-              height="300"
-            />
-          </div>
-        </div>
-        {/* end intro */}
-
-        <section className="homepage-intro__cards__wrapper">
-          <div className="homepage-intro__cards">
-
-            <div className="homepage-intro__card">
-              <h3 className="homepage-intro__card__title">
-                <a href="#" className="homepage-intro__card__link">Strategy</a>
-              </h3>
-              <p className="homepage-intro__card__desc">
-                An expert diagnosis of your business goals and web product to trace the best approach to the development and design of what you need! 
-              </p>
-
-              <button className="homepage-intro__card__button" aria-label="read more" />
-            </div>
-
-            <div className="homepage-intro__card">
-              <h3 className="homepage-intro__card__title">
-                <a href="#" className="homepage-intro__card__link">UI/UX Design</a>
-              </h3>
-              <p className="homepage-intro__card__desc">
-                User-friendly and beautifully crafted web products to elevate your brand while accounting for usability and performance for your web product.
-              </p>
-
-              <button className="homepage-intro__card__button" aria-label="read more" />
-            </div>
-
-            <div className="homepage-intro__card">
-              <h3 className="homepage-intro__card__title">
-                <a href="#" className="homepage-intro__card__link">Development</a>
-              </h3>
-              <p className="homepage-intro__card__desc">
-                You need a solid technological base to maintain and grow your web product. Our team of developers will guide you there!
-              </p>
-
-              <button className="homepage-intro__card__button" aria-label="read more" />
-            </div>
-          </div>
-        </section>
-
-        {/* end intro cards */}
       </section>
 
+      <section className="homepage-infographic__wrapper">
         
-        <section className="homepage-intro__infographic__wrapper">
+        <div className="homepage-infographic__content">
           
-          <div className="homepage-intro__infographic__content">
-            
-            <div className="homepage-intro__infographic__left">
-              <p className="homepage-intro__infographic__subtitle">We Transform</p>
-              <h1 className="homepage-intro__infographic__title">Your Ideas<br />Into Reality</h1>
-              <p className="homepage-intro__infographic__subtitle--small">By bringing Current Technologies with Your Business Goals</p>
-            </div>
-
-            <div className="homepage-intro__infographic__right">
-              <img
-                className="homepage-intro__infographic__img"
-                src="/assets/hp_manta_ray_infographic.png"
-                alt="Assess, Plan, Implement, Maintain, Evolve"
-                title="Assess, Plan, Implement, Maintain, Evolve"
-                width="300"
-                height="600"
-              />
-
-              <ul className="homepage-intro__infographic__list">
-                <li className="homepage-intro__infographic__item">
-                  Assess
-                </li>
-
-                <li className="homepage-intro__infographic__item">
-                  Plan
-                </li>
-
-                <li className="homepage-intro__infographic__item">
-                  Implement
-                </li>
-
-                <li className="homepage-intro__infographic__item">
-                  Maintain
-                </li>
-
-                <li className="homepage-intro__infographic__item">
-                  Evolve
-                </li> 
-              </ul>
-            </div>
+          <div className="homepage-infographic__left">
+            <p className="homepage-infographic__subtitle">We Transform</p>
+            <h1 className="homepage-infographic__title">Your Ideas<br />Into Reality</h1>
+            <p className="homepage-infographic__subtitle--small">By bringing Current Technologies with Your Business Goals</p>
           </div>
 
-        </section>
+          <div className="homepage-infographic__right">
+            <img
+              className="homepage-infographic__img"
+              src="/assets/hp_manta_ray_infographic.png"
+              alt="Assess, Plan, Implement, Maintain, Evolve"
+              title="Assess, Plan, Implement, Maintain, Evolve"
+              width="300"
+              height="600"
+            />
+
+            <ul className="homepage-infographic__list">
+              <li className="homepage-infographic__item">
+                Assess
+              </li>
+
+              <li className="homepage-infographic__item">
+                Plan
+              </li>
+
+              <li className="homepage-infographic__item">
+                Implement
+              </li>
+
+              <li className="homepage-infographic__item">
+                Maintain
+              </li>
+
+              <li className="homepage-infographic__item">
+                Evolve
+              </li> 
+            </ul>
+          </div>
+        </div>
+
+      </section>
+      {/* end infographic */}
+
+      <section className="homepage-case-studies__wrapper">
+        <div className="homepage-case-studies__content">
+
+          <div className="homepage-case-studies__text">
+            <h1 className="homepage-case-studies__title">Why Us?</h1>
+            <h2 className="homepage-case-studies__subtitle">We will guide you through the best approach towards having an outstanding web product.</h2>
+          </div>
+
+          <article className="homepage-case-study">
+            <a href="#" title="case study" className="homepage-case-study__img-link">
+              <img className="homepage-case-study__img" alt="Marzee Case Study" src="https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" width="300" height="200" />
+            </a>
+            
+            <h3 className="homepage-case-study__title">
+              <a href="#" className="homepage-case-study__link">
+                Facilisi nisl, egestas viverra felis. Ultrices vulputate ac quis a drerit.
+              </a>
+
+              <a href="#" className="homepage-case-study__link__cta">
+                View Case Study
+              </a>
+            </h3>
+          </article>
+
+          <article className="homepage-case-study">
+            <a href="#" title="case study" className="homepage-case-study__img-link">
+              <img className="homepage-case-study__img" alt="Marzee Case Study" src="https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" width="300" height="200" />
+            </a>
+            
+            <h3 className="homepage-case-study__title">
+              <a href="#" className="homepage-case-study__link">
+                Facilisi nisl, egestas viverra felis. Ultrices vulputate ac quis a drerit.
+              </a>
+
+              <a href="#" className="homepage-case-study__link__cta">
+                View Case Study
+              </a>
+            </h3>
+          </article>
+
+          <article className="homepage-case-study">
+            <a href="#" title="case study" className="homepage-case-study__img-link">
+              <img className="homepage-case-study__img" alt="Marzee Case Study" src="https://images.unsplash.com/photo-1593697821252-0c9137d9fc45?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" width="300" height="200" />
+            </a>
+            
+            <h3 className="homepage-case-study__title">
+              <a href="#" className="homepage-case-study__link">
+                Facilisi nisl, egestas viverra felis. Ultrices vulputate ac quis a drerit.
+              </a>
+
+              <a href="#" className="homepage-case-study__link__cta">
+                View Case Study
+              </a>
+            </h3>
+          </article>
+        </div>
+
+          <button className="button-main homepage-case-studies__cta" type="button" >View More of Our Work</button>
+
+      </section>
+      {/* end case studies */}
 
     </main>
     <footer className="footer">

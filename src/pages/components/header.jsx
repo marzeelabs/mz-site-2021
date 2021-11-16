a7a5901cd624a5e6e5dec30cb8d72b2701bd388b
Pedro Santos <pedro@marzeelabs.org>
2021-11-16 17:45:08 +0000

    Mobile menu fix

--- src/pages/components/header.jsx
+++ src/pages/components/header.jsx
@@ -1,17 +1,14 @@
+import { useState } from 'react';
 import Logo from './logo';
 const Header = () => { 
 
-  if (typeof window !== "undefined") {
-    window.addEventListener('load',function(){
-      const mobileMenu = document.querySelectorAll(".header-toggle");
-      const mobileDropdown = document.querySelectorAll(".header-menu");
 
-      mobileMenu[0].addEventListener('click', function(e) {
-        this.classList.toggle("is-open");
-        mobileDropdown[0].classList.toggle("is-visible");
-        document.body.classList.toggle("has-menu-open");
-      });
-    });
+  const [ isOpen, setIsOpen ] = useState(false);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    setIsOpen(!isOpen);
+    document.body.classList.toggle("has-menu-open");
   }
 
   return (
@@ -23,7 +20,7 @@ const Header = () => {
           <Logo/>
         </a>
 
-        <ul className="header-menu">
+        <ul className={isOpen ? 'header-menu is-visible' : 'header-menu'}>
           <li className="header-menu__item">
             <a className="header-menu__link" href="#">About</a>
           </li>
@@ -43,7 +40,7 @@ const Header = () => {
       </div>
 
       <div className="header-toggle__wrapper">
-        <button className="header-toggle" type="button" aria-label="open menu">
+        <button className={isOpen ? 'header-toggle is-open' : 'header-toggle'} type="button" aria-label="open menu" onClick={handleClick}>
           <span className="header-toggle__icon" aria-label="menu icon"></span>
         </button>
       </div>

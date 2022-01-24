53b2b3b061e45f836f165d150ed9b7dfae1a5f6c
Pedro Santos <pedro@marzeelabs.org>
2022-01-24 11:32:24 +0000

    about page WIP

---
+++ src/pages/pages-sections/about/about-team.jsx
@@ -0,0 +1,16 @@
+const AboutIntro = () =>
+
+ (
+    <>
+      <section className="about-team__wrapper">
+
+        <h1 className="title-display">
+          Le Team
+        </h1>
+
+      </section>
+
+    </>
+  );
+
+export default AboutIntro;

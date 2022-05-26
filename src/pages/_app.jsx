3582366b2b07d278512593f81f37ae0d8eecca7f
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2022-05-26 14:52:21 +0100

    small warning fixes

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,8 +6,8 @@ const MyApp = ({ Component, pageProps }) =>
  (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" ></link>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin ></link>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900&display=swap" rel="stylesheet"></link>
       </Head>
       <Component { ...pageProps } />
